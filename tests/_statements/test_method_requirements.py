da16cb8048b71c6596eead49e136add64a918ff0
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2023-05-01 20:41:34 +0200

    use parse from conftest, update tests to sets

--- tests/_statements/test_method_requirements.py
+++ tests/_statements/test_method_requirements.py
@@ -1,6 +1,4 @@
-import ast
 import sys
-import textwrap
 
 import pytest
 
@@ -12,207 +10,195 @@ match_statement = pytest.mark.skipif(
 )
 
 
-def _method_requirements(source):
-    source = textwrap.dedent(source)
-    root = ast.parse(source)
-    assert len(root.body) == 1
-    node = root.body[0]
-    if sys.version_info >= (3, 9):
-        print(ast.dump(node, include_attributes=True, indent=2))
-    else:
-        print(ast.dump(node, include_attributes=True))
-    return list(get_method_requirements(node))
-
-
-def test_staticmethod_requirements():
-    reqs = _method_requirements(
+def test_staticmethod_requirements(parse):
+    reqs = parse(
         """
         @staticmethod
         def function():
             return Class.attr
         """
     )
-    assert reqs == []
+    assert get_method_requirements(reqs) == set()
 
 
-def test_classmethod_requirements():
-    reqs = _method_requirements(
+def test_classmethod_requirements(parse):
+    reqs = parse(
         """
         @classmethod
         def function(cls, arg, **kwargs):
             return cls.attr + arg.argattr
         """
     )
-    assert reqs == ["attr"]
+    assert get_method_requirements(reqs) == {"attr"}
 
 
-def test_method_requirements():
-    reqs = _method_requirements(
+def testparse(parse):
+    reqs = parse(
         """
         def function(self, something):
             return self.a + self.b - something.other
         """
     )
-    assert reqs == ["a", "b"]
+    assert get_method_requirements(reqs) == {"a", "b"}
 
 
-def test_assign_method_requirements_none():
+def test_assignparse_none(parse):
     """
     ..code:: python
 
         Assign(expr* targets, expr value, string? type_comment)
     """
-    reqs = _method_requirements(
+    reqs = parse(
         """
         def fun(self):
             a = b
         """
     )
-    assert reqs == []
+    assert get_method_requirements(reqs) == set()
 
 
-def test_assign_method_requirements_target():
+def test_assignparse_target(parse):
     """
     ..code:: python
 
         Assign(expr* targets, expr value, string? type_comment)
     """
-    reqs = _method_requirements(
+    reqs = parse(
         """
         def fun(self):
             self.a = b
         """
     )
-    assert reqs == []
+    assert get_method_requirements(reqs) == set()
 
 
-def test_assign_method_requirements_value():
+def test_assignparse_value(parse):
     """
     ..code:: python
 
         Assign(expr* targets, expr value, string? type_comment)
     """
-    reqs = _method_requirements(
+    reqs = parse(
         """
         def fun(self):
             a = self.b
         """
     )
-    assert reqs == ["b"]
+    assert get_method_requirements(reqs) == {"b"}
 
 
-def test_assign_method_attribute_requirements_none():
-    reqs = _method_requirements(
+def test_assign_method_attribute_requirements_none(parse):
+    reqs = parse(
         """
         def fun(self):
             a.b = c
         """
     )
-    assert reqs == []
+    assert get_method_requirements(reqs) == set()
 
 
-def test_assign_method_attribute_requirements():
-    reqs = _method_requirements(
+def test_assign_method_attribute_requirements(parse):
+    reqs = parse(
         """
         def fun(self):
             self.a.b = c
         """
     )
-    assert reqs == ["a"]
+    assert get_method_requirements(reqs) == {"a"}
 
 
-def test_assign_method_star_requirements_none():
-    reqs = _method_requirements(
+def test_assign_method_star_requirements_none(parse):
+    reqs = parse(
         """
         def fun(self):
             *a = c
         """
     )
-    assert reqs == []
+    assert get_method_requirements(reqs) == set()
 
 
-def test_assign_method_star_requirements():
-    reqs = _method_requirements(
+def test_assign_method_star_requirements(parse):
+    reqs = parse(
         """
         def fun(self):
             *self.a = c
         """
     )
-    assert reqs == []
+    assert get_method_requirements(reqs) == set()
 
 
-def test_assign_method_star_attribute_requirements_none():
-    reqs = _method_requirements(
+def test_assign_method_star_attribute_requirements_none(parse):
+    reqs = parse(
         """
         def fun(self):
             *a.b = c
         """
     )
-    assert reqs == []
+    assert get_method_requirements(reqs) == set()
 
 
-def test_assign_method_star_attribute_requirements():
-    reqs = _method_requirements(
+def test_assign_method_star_attribute_requirements(parse):
+    reqs = parse(
         """
         def fun(self):
             *self.a.b = c
         """
     )
-    assert reqs == ["a"]
+    assert get_method_requirements(reqs) == {"a"}
 
 
-def test_assign_method_subscript_requirements_none():
-    reqs = _method_requirements(
+def test_assign_method_subscript_requirements_none(parse):
+    reqs = parse(
         """
         def fun(self):
             a[b] = c
         """
     )
-    assert reqs == []
+    assert get_method_requirements(reqs) == set()
 
 
-def test_assign_method_subscript_requirements_source():
-    reqs = _method_requirements(
+def test_assign_method_subscript_requirements_source(parse):
+    reqs = parse(
         """
         def fun(self):
             self.a[b] = c
         """
     )
-    assert reqs == ["a"]
+    assert get_method_requirements(reqs) == {"a"}
 
 
-def test_assign_method_subscript_requirements_key():
-    reqs = _method_requirements(
+def test_assign_method_subscript_requirements_key(parse):
+    reqs = parse(
         """
         def fun(self):
             a[self.b] = c
         """
     )
-    assert reqs == ["b"]
+    assert get_method_requirements(reqs) == {"b"}
 
 
-def test_assign_method_tuple_requirements_none():
-    reqs = _method_requirements(
+def test_assign_method_tuple_requirements_none(parse):
+    reqs = parse(
         """
         def fun(self):
             a, b[c], d.e, *f = g
         """
     )
-    assert reqs == []
+    assert get_method_requirements(reqs) == set()
 
 
-def test_assign_method_tuple_requirements():
-    reqs = _method_requirements(
+def test_assign_method_tuple_requirements(parse):
+    reqs = parse(
         """
         def fun(self):
             self.a, self.b[self.c], self.d.e, *self.f = self.g
         """
     )
-    assert reqs == ["b", "c", "d", "g"]
+    assert get_method_requirements(reqs) == {"b", "c", "d", "g"}
 
 
-def test_method_requirements_inner_function():
-    reqs = _method_requirements(
+def testparse_inner_function(parse):
+    reqs = parse(
         """
         def fun(self):
             def inner():
@@ -220,93 +206,93 @@ def test_method_requirements_inner_function():
             return inner()
         """
     )
-    assert reqs == ["a"]
+    assert get_method_requirements(reqs) == {"a"}
 
 
-def test_method_requirements_formatted_value():
-    reqs = _method_requirements(
+def testparse_formatted_value(parse):
+    reqs = parse(
         """
         def fun(self):
             return f"{self.a} {self.b} {self.c}"
         """
     )
-    assert reqs == ["a", "b", "c"]
+    assert get_method_requirements(reqs) == {"a", "b", "c"}
 
 
-def test_method_requirements_list_comp():
-    reqs = _method_requirements(
+def testparse_list_comp(parse):
+    reqs = parse(
         """
         def fun(self):
             return [self.a for self.b.c in self.d]
         """
     )
-    assert reqs == ["a", "b", "d"]
+    assert get_method_requirements(reqs) == {"a", "b", "d"}
 
 
-def test_method_requirements_set_comp():
-    reqs = _method_requirements(
+def testparse_set_comp(parse):
+    reqs = parse(
         """
         def fun(self):
             return {self.a for self.b.c in self.d}
         """
     )
-    assert reqs == ["a", "b", "d"]
+    assert get_method_requirements(reqs) == {"a", "b", "d"}
 
 
-def test_method_requirements_dict_comp():
-    reqs = _method_requirements(
+def testparse_dict_comp(parse):
+    reqs = parse(
         """
         def fun(self):
             return {self.a: self.b for self.c.d in self.e}
         """
     )
-    assert reqs == ["a", "b", "c", "e"]
+    assert get_method_requirements(reqs) == {"a", "b", "c", "e"}
 
 
-def test_method_requirements_generator_exp():
-    reqs = _method_requirements(
+def testparse_generator_exp(parse):
+    reqs = parse(
         """
         def fun(self):
             return (self.a for self.b.c in self.d)
         """
     )
-    assert reqs == ["a", "b", "d"]
+    assert get_method_requirements(reqs) == {"a", "b", "d"}
 
 
-def test_method_requirements_lambda_default():
-    reqs = _method_requirements(
+def testparse_lambda_default(parse):
+    reqs = parse(
         """
         def fun(self):
             return lambda x=self.a: self.b
         """
     )
-    assert reqs == ["a", "b"]
+    assert get_method_requirements(reqs) == {"a", "b"}
 
 
-def test_method_requirements_with():
-    reqs = _method_requirements(
+def testparse_with(parse):
+    reqs = parse(
         """
         def fun(self):
             with self.a as self.b.c:
                 pass
         """
     )
-    assert reqs == ["a", "b"]
+    assert get_method_requirements(reqs) == {"a", "b"}
 
 
-def test_method_requirements_ann_assign():
-    reqs = _method_requirements(
+def testparse_ann_assign(parse):
+    reqs = parse(
         """
         def fun(self):
             self.a: self.b = self.c
         """
     )
-    assert reqs == ["b", "c"]
+    assert get_method_requirements(reqs) == {"b", "c"}
 
 
 @match_statement
-def test_method_requirements_match_statement():
-    reqs = _method_requirements(
+def testparse_match_statement(parse):
+    reqs = parse(
         """
         def fun(self):
             match self.a:
@@ -314,4 +300,4 @@ def test_method_requirements_match_statement():
                     pass
         """
     )
-    assert reqs == ["a", "b"]
+    assert get_method_requirements(reqs) == {"a", "b"}
