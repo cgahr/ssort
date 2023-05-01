d23ca4866fc05f9e5fcecbc1ef33259445c993d1
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2023-05-01 20:24:50 +0200

    cleanup NodeVisitor

--- src/ssort/_statements/_bindings.py
+++ src/ssort/_statements/_bindings.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import ast
-from typing import Sequence
 
 from ._node_visitor import SmartNodeVisitor
 
@@ -9,68 +8,66 @@ __all__ = ["get_bindings"]
 
 
 class Bindings(SmartNodeVisitor):
-    def visit_FunctionDef(self, node: ast.FunctionDef | ast.AsyncFunctionDef):
-        self.smart_visit(node.decorator_list)
-        self.smart_append(node.name)
-        # body is missing
+    def _store_name_visit_rest(
+        self, node: ast.ExceptHandler | ast.MatchAs | ast.MatchStar
+    ):
+        for field, child in ast.iter_fields(node):
+            if field == "name" and node.name:
+                self.stack.add(node.name)
+            else:
+                self.smart_visit(child)
+
+    visit_ExceptHandler = _store_name_visit_rest
+    visit_MatchAs = _store_name_visit_rest
+    visit_MatchStar = _store_name_visit_rest
+
+    def _store_names(self, node: ast.Global | ast.Nonlocal):
+        self.stack |= set(node.names)
+
+    visit_Global = _store_names
+    visit_Nonlocal = _store_names
+
+    def _store_name_skip_body_visit_rest(
+        self,
+        node: ast.AsyncFunctionDef
+        | ast.ClassDef
+        | ast.FunctionDef
+        | ast.Lambda,
+    ):
+        for field, child in ast.iter_fields(node):
+            if field == "name":
+                self.stack.add(child)
+            elif field != "body":
+                self.smart_visit(child)
+
+    visit_FunctionDef = _store_name_skip_body_visit_rest
+    visit_AsyncFunctionDef = _store_name_skip_body_visit_rest
+    visit_ClassDef = _store_name_skip_body_visit_rest
+
+    def visit_Lambda(self, node: ast.Lambda):
         self.smart_visit(node.args)
-        self.smart_visit(node.returns)
-
-    visit_AsyncFunctionDef = visit_FunctionDef
-
-    def visit_ClassDef(self, node: ast.ClassDef):
-        self.smart_visit(node.decorator_list)
-        self.smart_visit(node.bases)
-        self.smart_visit(node.keywords)
-        self.smart_append(node.name)
-        # missing keywords, starargs, body
 
     def visit_Import(self, node):
-        for name in node.names:
-            if name.asname:
-                self.smart_append(name.asname)
-            else:
-                root, *rest = name.name.split(".", 1)
-                self.smart_append(root)
+        self.stack |= set(
+            n.asname if n.asname else n.name.split(".")[0] for n in node.names
+        )
 
     def visit_ImportFrom(self, node):
-        for name in node.names:
-            self.smart_append(name.asname if name.asname else name.name)
-
-    def visit_Global(self, node: ast.Global | ast.Nonlocal):
-        self.smart_append(node.names)
-
-    visit_Nonlocal = visit_Global
-
-    def visit_Lambda(self, node):
-        self.smart_visit(node.args)
-
-    # def visit_alias(self, node: ast.alias):
-    #     self.smart_append(node.asname if node.asname else node.name)
+        self.stack |= set(n.asname if n.asname else n.name for n in node.names)
 
     def visit_Name(self, node: ast.Name):
         if isinstance(node.ctx, ast.Store):
-            self.smart_append(node.id)
-
-    def visit_ExceptHandler(self, node: ast.ExceptHandler):
-        self.smart_visit(node.type)
-        self.smart_append(node.name)
-        self.smart_visit(node.body)
-
-    def visit_MatchStar(self, node: ast.MatchStar):
-        self.smart_append(node.name)
+            self.stack.add(node.id)
 
     def visit_MatchMapping(self, node: ast.MatchMapping):
-        self.smart_visit(node.keys)
-        self.smart_visit(node.patterns)
-        self.smart_append(node.rest)
-
-    def visit_MatchAs(self, node: ast.MatchAs):
-        self.smart_visit(node.pattern)
-        self.smart_append(node.name)
+        for field, child in ast.iter_fields(node):
+            if field == "rest":
+                self.stack.add(child)
+            else:
+                self.smart_visit(child)
 
 
-def get_bindings(node: ast.AST):
+def get_bindings(node: ast.AST) -> set[str]:
     bindings = Bindings()
     bindings.visit(node)
-    yield from bindings.stack
+    return bindings.stack
