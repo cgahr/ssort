8eb4cb789861cf574fd2583431f97edc98804f37
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2023-05-01 20:23:23 +0200

    make stack a set, remove smart_append

--- src/ssort/_statements/_node_visitor.py
+++ src/ssort/_statements/_node_visitor.py
@@ -1,20 +1,12 @@
+from __future__ import annotations
+
 import ast
 from typing import Sequence
 
 
 class SmartNodeVisitor(ast.NodeVisitor):
     def __init__(self):
-        self.stack = []
-
-    def smart_append(self, name: list[str] | str | None):
-        if name is None:
-            return
-
-        if isinstance(name, list):
-            self.stack.extend(name)
-
-        else:
-            self.stack.append(name)
+        self.stack: set[str] = set()
 
     def smart_visit(self, node: Sequence[ast.AST] | ast.AST | None):
         if node is None:
