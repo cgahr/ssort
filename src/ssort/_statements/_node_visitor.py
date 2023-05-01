bef8a0d921b68bd76f4b725f6b8dbed4fb5e952a
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2023-05-01 20:48:59 +0200

    add types

--- src/ssort/_statements/_node_visitor.py
+++ src/ssort/_statements/_node_visitor.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 import ast
-from typing import Sequence
+from typing import Generic, Hashable, Sequence, TypeVar
+
+__all__ = ["SmartNodeVisitor"]
+
+T = TypeVar("T", bound=Hashable)
 
 
-class SmartNodeVisitor(ast.NodeVisitor):
-    def __init__(self):
-        self.stack: set[str] = set()
+class SmartNodeVisitor(ast.NodeVisitor, Generic[T]):
+    def __init__(self) -> None:
+        self.stack: set[T] = set()
 
-    def smart_visit(self, node: Sequence[ast.AST] | ast.AST | None):
+    def smart_visit(self, node: Sequence[ast.AST] | ast.AST | None) -> None:
         if node is None:
             return
 
